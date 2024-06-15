f65f02f1adf6b7e2b34fa4c94e001bdad82caed8
miggymofongo2 <contact@miguelalmodo.com>
2024-06-15 00:16:44 -0400

    starting out my quote shuffler v2

---
+++ src/Components/Quote/quote.js
@@ -0,0 +1,85 @@
+const quotes = [
+    {
+      quote: "So many places I want to see... I know I'll get there someday. I'll find a way somehow. I'm sure of it.",
+      author: "ARIEL",
+      game: "Kingdom Hearts"
+    },
+    {
+      quote: "You be careful, now, both of you.",
+      author: "DAISY DUCK",
+      game: "Kingdom Hearts"
+    },
+    {
+      quote: "All for one, and one for all!",
+      author: "GOOFY",
+      game: "Kingdom Hearts"
+    },
+    {
+      quote: "Cricket's the name. Jiminy Cricket, at your service.",
+      author: "JIMINY CRICKET",
+      game: "Kingdom Hearts"
+    },
+    {
+      quote: "I want to be a part of your life no matter what. That's all.",
+      author: "KAIRI",
+      game: "Kingdom Hearts III"
+    },
+    {
+      quote: "There will always be a door to the light.",
+      author: "KING MICKEY",
+      game: "Kingdom Hearts II"
+    },
+
+    {
+      quote: "We may never meet again, but we'll never forget each other.",
+      author:"LEON",
+      game: "Kingdom Hearts"
+    }, 
+
+    {
+      quote: "The heart of all kingdoms, the heart of all that lives. A dominion fit to be called Kingdom Hearts must be MY dominion.",
+      author: "MALEFICENT",
+      game: "Kingdom Hearts II"
+    }, 
+
+    {
+      quote: "The name is Merlin! I'm a powerful wizard, and I'll have you know a lot of wisdom comes with age.",
+      author: "MERLIN",
+      game: "Kingdom Hearts Birth by Sleep"
+    }, 
+
+    {
+      quote: "Kid, I got two words of advice for you: Attack!" ,
+      author: "PHILOCTETES",
+      game: "Kingdom Hearts"
+    }, 
+
+    {
+      quote: "What really makes someone a hero isn't what they wear or what they say it's the things they do and how they treat everyone.",
+      author: "QUEEN MINNIE MOUSE",
+      game: "Kingdom Hearts Birth by Sleep"
+    }, 
+
+    {
+      quote: "I'm going to face my past.",
+      author: "SIMBA",
+      game:"Kingdom Hearts II"
+    }, 
+
+    {
+      quote: "My friends are my power!",
+      author: "SORA",
+      game:"Kingdom Hearts"
+    }, 
+
+    {
+      quote: "But the girl could prove useful. And I've got the Heartless on my side. Triton, my old friend...Your day is coming.",
+      author: "URSULA",
+      game: "Kingdom Hearts"
+    }
+
+  
+  ];
+
+export default quotes;
+  
