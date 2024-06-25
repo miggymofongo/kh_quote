9ae74deafe1f3576050b5f55e69331ccc2d37551
miggymofongo2 <contact@miguelalmodo.com>
2024-06-25 01:58:51 -0400

    fixed some comment

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+// to do : change header to matchgame oflife
 export default function Header() {
    
     return (
